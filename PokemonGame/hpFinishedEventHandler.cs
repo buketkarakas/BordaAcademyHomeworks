cd4e69012c86a996e703d65986fbd3afb9a23b39
Buket Karakaş <buketkarakas8@gmail.com>
2020-10-01 00:18:09 +0300

    Create hpFinishedEventHandler.cs

---
+++ PokemonGame/hpFinishedEventHandler.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonGame
+{
+
+    public delegate void HpFinishedEventHandler(Object sender, HpFinishedEventArgs e);
+}
